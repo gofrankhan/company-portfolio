11922d7eaed89c25f5c126fa31e7880c90860a31
Md. Gofran Khan <gofran0123@gmail.com>
2025-09-06 23:40:15 +0600

    index file added

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,19 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/contact', [ContactController::class, 'index']);
+// Route::get('/contact', [ContactController::class, 'index']);
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        $users = User::all();
-        $users = DB::table('users')->get();
-    return view('dashboard', compact('users'));})->name('dashboard');
-});
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         $users = User::all();
+//         $users = DB::table('users')->get();
+//     return view('dashboard', compact('users'));})->name('dashboard');
+// });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+
+    // $users = User::all();
+    // $users = DB::table('users')->get();
+
+    return view('admin.index');
+})->name('dashboard');
